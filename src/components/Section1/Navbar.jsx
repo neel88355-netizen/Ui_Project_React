979fe333dc701958c3fd1bd6797a89ce9a046d28
neel88355-netizen <neel88355@gmail.com>
2026-05-26 21:56:47 +0530

    updated project

--- src/components/Section1/Navbar.jsx
+++ src/components/Section1/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div>
       <div className="flex items-center justify-between py-8 ">
-        <h4 className='bg-gray-500 text-white px-6 py-2 uppercase rounded-full text-sm'>Target Audience</h4>
+        <h4 className='bg-black text-white px-6 py-2 uppercase rounded-full text-sm'>Target Audience</h4>
         <button className='bg-gray-200 px-6 py-2 uppercase rounded-full tracking-wider text-sm'>digital banking plateform</button>
       </div>
     </div>
